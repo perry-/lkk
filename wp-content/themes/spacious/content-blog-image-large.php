7157037f86b69e5be150cf56eb8927f665906228
Alexander Perry <alexander.perry@bouvet.no>
2015-07-02 18:24:01 +0200

    Removed date and comments spans from content-image-large

--- wp-content/themes/spacious/content-blog-image-large.php
+++ wp-content/themes/spacious/content-blog-image-large.php
@@ -48,15 +48,9 @@
 	<footer class="entry-meta-bar clearfix">	        			
 		<div class="entry-meta clearfix">
 			<span class="by-author author vcard"><a class="url fn n" href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>"><?php the_author(); ?></a></span>
-			<span class="date updated"><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( get_the_time() ); ?>"><?php the_time( get_option( 'date_format' ) ); ?></a></span>
-			<?php if( has_category() ) { ?>
-       		<!--<span class="category"><?php the_category(', '); ?></span> -->
-       	<?php } ?>
-				<?php if ( comments_open() ) { ?>
-       		<span class="comments"><?php comments_popup_link( __( 'No Comments', 'spacious' ), __( '1 Comment', 'spacious' ), __( '% Comments', 'spacious' ), '', __( 'Comments Off', 'spacious' ) ); ?></span>
-       	<?php } ?> 
-       	<?php edit_post_link( __( 'Edit', 'spacious' ), '<span class="edit-link">', '</span>' ); ?>
-			<span class="read-more-link"><a class="read-more" href="<?php the_permalink(); ?>"><?php _e( 'Read more', 'spacious' ); ?></a></span>
+
+       	<?php edit_post_link( __( 'Rediger', 'spacious' ), '<span class="edit-link">', '</span>' ); ?>
+			<span class="read-more-link"><a class="read-more" href="<?php the_permalink(); ?>"><?php _e( 'Les mer', 'spacious' ); ?></a></span>
 		</div>
 	</footer>
 	<?php
