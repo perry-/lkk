f13d501499729da3de6da9f1f3f25b8e49703e28
Øystein Grændsen <oysteigr@gmail.com>
2015-08-24 21:08:26 +0200

    Codecleaning

--- wp-content/themes/spacious/inc/GMapsOverview.php
+++ wp-content/themes/spacious/inc/GMapsOverview.php
@@ -58,10 +58,8 @@
 function initialize() {
 
     var myVariable = <?php echo(json_encode($kodePlaces)); ?>;
-    var mySize = <?php echo(sizeof($kodePlaces)); ?>;
 
 
-  var markers = [];
   var map = new google.maps.Map(document.getElementById('map-canvas'), {
     mapTypeId: google.maps.MapTypeId.ROADMAP
   });
@@ -71,60 +69,12 @@ function initialize() {
       new google.maps.LatLng(58.532423, 28.2631));
   map.fitBounds(defaultBounds);
 
-  // Create the search box and link it to the UI element.
-  var input = /** @type {HTMLInputElement} */(
-      document.getElementById('pac-input'));
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-  var searchBox = new google.maps.places.SearchBox(
-    /** @type {HTMLInputElement} */(input));
-
-  // [START region_getplaces]
-  // Listen for the event fired when the user selects an item from the
-  // pick list. Retrieve the matching places for that item.
-  google.maps.event.addListener(searchBox, 'places_changed', function() {
-    var places = searchBox.getPlaces();
-
-    if (places.length == 0) {
-      return;
-    }
-    document.getElementById('position_field').value = input.value;
-    document.getElementById('field_lat').value = places[0].geometry.location.lat();
-    document.getElementById('field_long').value = places[0].geometry.location.lng();
-    for (var i = 0, marker; marker = markers[i]; i++) {
-      marker.setMap(null);
-    }
-
-    // For each place, get the icon, place name, and location.
-    markers = [];
-    var bounds = new google.maps.LatLngBounds();
-    for (var i = 0, place; place = places[i]; i++) {
-      var image = {
-        url: place.icon,
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25)
-      };
-
-      // Create a marker for each place.
-      var marker = new google.maps.Marker({
-        map: map,
-        icon: image,
-        title: place.name,
-        position: place.geometry.location
-      });
-
-      markers.push(marker);
-
-      bounds.extend(place.geometry.location);
-    }
-
-    map.fitBounds(bounds);
-  });
+ 
+ 
   // [END region_getplaces]
         //document.write(myVariable[0].name);
         var arrayLength = myVariable.length;
+        var bounds = new google.maps.LatLngBounds();
     for (index = 0; index < arrayLength; ++index) {
 
       var pos = new google.maps.LatLng(myVariable[index].lat, myVariable[index].long); 
@@ -133,15 +83,10 @@ function initialize() {
             map: map,
             title: myVariable[index].name
         });
+        bounds.extend(new google.maps.LatLng(myVariable[index].lat, myVariable[index].long));
     }
 
 
-  // Bias the SearchBox results towards places that are within the bounds of the
-  // current map's viewport.
-  google.maps.event.addListener(map, 'bounds_changed', function() {
-    var bounds = map.getBounds();
-    searchBox.setBounds(bounds);
-  });
 
 
 }
