d80434b6f98067975c271ae3d3b8e594ee47c4e8
Alexander Perry <alexander.perry@bouvet.no>
2015-10-20 19:07:42 +0200

    Added accordion styling and JS for FAQ

--- wp-content/themes/spacious/inc/lkk-functions.php
+++ wp-content/themes/spacious/inc/lkk-functions.php
@@ -1,9 +1,8 @@
-<?php 
+<?php
 
 /**
 * List all function files used in LKK
 */
-
 
 function add_scripts() {
   wp_enqueue_script('google-maps', 'https://maps.googleapis.com/maps/api/js?v=3.exp&signed_in=true&libraries=places');
@@ -12,7 +11,12 @@ function add_scripts() {
       'lkk-checkbox', // name your script so that you can attach other scripts and de-register, etc.
       SPACIOUS_JS_URL . '/checkbox.js', // this is the location of your script file
       array('jquery') // this array lists the scripts upon which your script depends
-  );  
+  );
+  wp_enqueue_script(
+      'lkk-accordion', // name your script so that you can attach other scripts and de-register, etc.
+      SPACIOUS_JS_URL . '/accordion.js', // this is the location of your script file
+      array('jquery') // this array lists the scripts upon which your script depends
+  );
 }
 add_action('wp_enqueue_scripts', 'add_scripts');
 
---
+++ wp-content/themes/spacious/js/accordion.js
@@ -0,0 +1,5 @@
+jQuery(document).ready(function($) {
+    $(".qa-faq-anchor").bind("click", function(){
+        $(this).toggleClass("active");
+    });
+});
