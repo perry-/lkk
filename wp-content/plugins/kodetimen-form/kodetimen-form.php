ca4b32fd27aeb04292336727a7d19a5c912a0cff
Alexander Perry <alexander.perry@bouvet.no>
2016-09-07 17:21:39 +0200

    Code clean-up for kodetimen-form, added styling

--- wp-content/plugins/kodetimen-form/kodetimen-form.php
+++ wp-content/plugins/kodetimen-form/kodetimen-form.php
@@ -12,46 +12,67 @@ Author: LKK
 function html_form_code() {
 	echo '<form class="kodetimen-form" action="' . esc_url( $_SERVER['REQUEST_URI'] ) . '" method="post">';
 
-    echo '<fieldset>';
-    echo '<legend>Skole / barnehage</legend>';
-    
+	echo '<fieldset class="kodetimen-form__fieldset">';
+    echo '<legend class="kodetimen-form__legend">Skole / barnehage</legend>';
+
+	echo '<div class="kodetimen-form__field">';
     echo  '<label for="autocomplete">Søk på skole/barnehage eller adresse</label>';
-    echo  '  <input id="autocomplete" placeholder="Skole-/barnehagenavn eller adresse" type="text"></input>';
+    echo  '  <input class="kodetimen-form__input" id="autocomplete" placeholder="" type="text"></input>';
+	echo '</div>';
 
+	echo '<div class="kodetimen-form__field">';
     echo  '<label for="street_number">Gate</label>';
-    echo  '<input id="street_number" disabled="true"></input>';
+    echo  '<input class="kodetimen-form__input" id="street_number"></input>';
+	echo '</div>';
 
+	echo '<div class="kodetimen-form__field">';
     echo  '<label for="route">Gatenummer</label>';
-    echo  '<input id="route" disabled="true"></input>';
+    echo  '<input class="kodetimen-form__input" id="route" ></input>';
+	echo '</div>';
 
+	echo '<div class="kodetimen-form__field">';
     echo  '<label for="locality">By</label>';
-    echo  '<input id="locality" disabled="true"></input>';
+    echo  '<input class="kodetimen-form__input" id="locality" ></input>';
+	echo '</div>';
 
+	echo '<div class="kodetimen-form__field">';
     echo  '<label for="administrative_area_level_1">Fylke</label>';
-    echo  '<input id="administrative_area_level_1" disabled="true"></input>';
+    echo  '<input class="kodetimen-form__input" id="administrative_area_level_1" ></input>';
+	echo '</div>';
 
-    echo  '<label for="postal_code">Zip code</label>';
-    echo  '<input id="postal_code" disabled="true"></input>';
+	echo '<div class="kodetimen-form__field">';
+    echo  '<label for="postal_code">Postnummer</label>';
+    echo  '<input class="kodetimen-form__input" id="postal_code" ></input>';
+	echo '</div>';
 
+	echo '<div class="kodetimen-form__field">';
     echo  '<label for="school">Skole</label>';
-    echo  '<input id="school"></input>';
+    echo  '<input class="kodetimen-form__input" id="school"></input>';
+	echo '</div>';
 
     echo '</fieldset>';
 
-    echo '<fieldset>';
-    echo '<legend>Kontaktperson</legend>';
+    echo '<fieldset class="kodetimen-form__fieldset">';
+    echo '<legend class="kodetimen-form__legend">Kontaktperson</legend>';
+
+	echo '<div class="kodetimen-form__field">';
 	echo '<label for="name">Navn (påkrevd)</label>';
-	echo '<input type="text" id="name" name="cf-name" pattern="[a-zA-Z0-9 ]+" value="' . ( isset( $_POST["cf-name"] ) ? esc_attr( $_POST["cf-name"] ) : '' ) . '"/>';
+	echo '<input class="kodetimen-form__input" type="text" id="name" name="cf-name" pattern="[a-zA-Z0-9 ]+" value="' . ( isset( $_POST["cf-name"] ) ? esc_attr( $_POST["cf-name"] ) : '' ) . '"/>';
+	echo '</div>';
 
+	echo '<div class="kodetimen-form__field">';
 	echo '<label for="email">E-post (påkrevd)</label>';
-	echo '<input type="email" id="email" name="cf-email" value="' . ( isset( $_POST["cf-email"] ) ? esc_attr( $_POST["cf-email"] ) : '' ) . '"/>';
+	echo '<input class="kodetimen-form__input" type="email" id="email" name="cf-email" value="' . ( isset( $_POST["cf-email"] ) ? esc_attr( $_POST["cf-email"] ) : '' ) . '"/>';
+	echo '</div>';
 
+	echo '<div class="kodetimen-form__field">';
 	echo '<label for="age">Alder</label>';
-	echo '<input type="number" id="age" name="cf-age" value="' . ( isset( $_POST["cf-age"] ) ? esc_attr( $_POST["cf-age"] ) : '' ) . '"/>';
+	echo '<input class="kodetimen-form__input" type="number" id="age" name="cf-age" value="' . ( isset( $_POST["cf-age"] ) ? esc_attr( $_POST["cf-age"] ) : '' ) . '"/>';
+	echo '</div>';
 
     echo '</fieldset>';
 
-	echo '<input type="submit" name="cf-submitted" value="Send">';
+	echo '<button class="kodetimen-form__button" type="submit" name="cf-submitted">Send</button>';
 
 	echo '</form>';
 }
@@ -63,11 +84,16 @@ function wp_kodetimen_enqueue_scripts()
     if ( !wp_script_is( 'google-maps', 'registered' ) ) {
         wp_register_script( 'google-maps', ( is_ssl() ? 'https' : 'http' ) . '://maps.googleapis.com/maps/api/js?key=AIzaSyA_VKoV6XVMReOu2b6wpTTUwYFyQKkKnPk&libraries=places&language=no', array( 'jquery' ), false );
     }
+	$plugin_url = plugin_dir_url( __FILE__ );
+
 
     //enqueue google maps api if not already enqueued
     if ( !wp_script_is( 'google-maps', 'enqueued' ) ) {
         wp_enqueue_script( 'google-maps' );
     }
+
+    wp_register_style( 'kodetimen-style', plugins_url( '/css/kodetimen-form.css', __FILE__ ));
+	wp_enqueue_style( 'kodetimen-style' );
 
     wp_register_script( 'kodetimen-adress', plugins_url( '/js/kodetimen-adress.js', __FILE__ ));
     wp_enqueue_script( 'kodetimen-adress' );
