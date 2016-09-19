afd7d35ebb952b7fb826debdf767c3dcd135533a
Alexander Perry <alexander.perry@bouvet.no>
2016-09-19 21:12:57 +0200

    feat: Use multiple fields to populate city value

--- wp-content/plugins/kodetimen-form/js/kodetimen-adress.js
+++ wp-content/plugins/kodetimen-form/js/kodetimen-adress.js
@@ -6,10 +6,12 @@ jQuery(document).ready(function($) {
       route: 'long_name',
       locality: 'long_name',
       administrative_area_level_1: 'short_name',
+	  administrative_area_level_2: 'long_name',
       postal_code: 'short_name'
     };
 
     function fillInAddress(place) {
+		console.log(place);
 		$(".kodetimen-form")[0].reset();
 		document.getElementById('kodetimen_lat').value = place.geometry.location.lat();
 		document.getElementById('kodetimen_long').value = place.geometry.location.lng();
@@ -30,18 +32,18 @@ jQuery(document).ready(function($) {
 			document.getElementById('school').value = place.name;
 		}
 
-      for (var component in componentForm) {
-        document.getElementById(component).value = '';
-        document.getElementById(component).disabled = false;
-      }
-
       // Get each component of the address from the place details
       // and fill the corresponding field on the form.
       for (var i = 0; i < place.address_components.length; i++) {
         var addressType = place.address_components[i].types[0];
         if (componentForm[addressType]) {
-          var val = place.address_components[i][componentForm[addressType]];
-          document.getElementById(addressType).value = val;
+			var val = place.address_components[i][componentForm[addressType]];
+
+			if(addressType === "administrative_area_level_2"){
+				document.getElementById("locality").value = val;
+			} else {
+				document.getElementById(addressType).value = val;
+			}
         }
       }
     }
