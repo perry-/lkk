11b6249b1bf259b7fa1b5873de579723bbed9966
Alexander Perry <alexander.perry@bouvet.no>
2016-10-06 12:12:43 +0200

    fix: Do not reset form after adress search

--- wp-content/plugins/kodetimen-form/js/kodetimen-adress.js
+++ wp-content/plugins/kodetimen-form/js/kodetimen-adress.js
@@ -11,7 +11,6 @@ jQuery(document).ready(function($) {
     };
 
     function fillInAddress(place) {
-		$(".kodetimen-form")[0].reset();
 		document.getElementById('kodetimen_lat').value = place.geometry.location.lat();
 		document.getElementById('kodetimen_long').value = place.geometry.location.lng();
 
